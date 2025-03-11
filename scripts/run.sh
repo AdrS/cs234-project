83df0920017f43586e4f9d3b749f85eca7ee1af9
molofsky <molofsky@stanford.edu>
2025-03-11 12:27:11 -0700

    fixing bug in scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,4 @@ JOB_NAME="${1}_${2}_${3}"
 OUTFILE="${JOB_NAME}.%j.out"
 ERRFILE="${JOB_NAME}.%j.err"
 
-sbatch -J "${JOB_NAME}" -o "${OUTFILE}" -e "${ERRFILE}" scripts/train.sh $1 $2 $3 -steps 10000000
+sbatch -J "${JOB_NAME}" -o "${OUTFILE}" -e "${ERRFILE}" scripts/train.sh $1 $2 $3
