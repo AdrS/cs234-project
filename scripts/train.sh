fe7e61cf888d3a816389180e97da74f626f43fe7
molofsky <molofsky@stanford.edu>
2025-03-11 12:26:37 -0700

    universal script for queuing models

---
+++ scripts/train.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=08:00:00                      # Wall time limit (hh:mm:ss)
+#SBATCH --nodes=1                            # Number of nodes
+#SBATCH --ntasks=1                           # Number of tasks
+#SBATCH --cpus-per-task=4                    # CPU cores per task
+#SBATCH --mem=16G                            # Memory per node
+
+MODEL="$1"
+ENV="$2"
+SIZE="$3"
+
+srun python example.py --environment "$ENV" --algorithm "$MODEL" --maze_size "$SIZE" --run_name "${1}_${2}_${3}"
