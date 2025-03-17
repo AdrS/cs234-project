eb22bcc928a463c111dc2c09c042c154d44a90aa
Adrian Stoll <adrs@umich.edu>
2025-03-16 21:48:09 -0700

    Show heatmap of policy differences

--- compare_policies.py
+++ compare_policies.py
@@ -42,6 +42,7 @@ def get_action_distribution(
     episode_start: Optional[np.ndarray] = None,
 ) -> tuple[np.ndarray, Optional[tuple[np.ndarray, ...]]]:
     # Switch to eval mode (this affects batch norm / dropout)
+
     self.set_training_mode(False)
     obs_tensor, vectorized_env = self.obs_to_tensor(observation)
     with th.no_grad():
@@ -102,7 +103,15 @@ def plot_kl_divergence_distribution(
     plt.title(f"{title_prefix} CDF of KL-Divergence for Y-force")
     plt.grid(True)
     plt.savefig(os.path.join(output_dir, file_prefix + "-y-force-kl-divergence.png"))
-    # TODO: combined plot
+
+def norm(x):
+    return np.sqrt(x.dot(x))
+
+# https://stackoverflow.com/questions/2827393
+def angle_between(x, y):
+    return np.degrees(
+        np.arccos(np.clip(np.dot(x / norm(x), y / norm(y)), -1.0, 1.0))
+    )
 
 
 # TODO: swap the order so it's trial - control and make sure the labels and
@@ -116,15 +125,6 @@ def plot_action_difference_distribution(
     output_dir,
     num_samples=1000,
 ):
-    def norm(x):
-        return np.sqrt(x.dot(x))
-
-    # https://stackoverflow.com/questions/2827393
-    def angle_between(x, y):
-        return np.degrees(
-            np.arccos(np.clip(np.dot(x / norm(x), y / norm(y)), -1.0, 1.0))
-        )
-
     norm_diffs = []
     x_diffs = []
     y_diffs = []
@@ -160,7 +160,69 @@ def plot_action_difference_distribution(
     plt.title(f"{title_prefix} Force Direction Delta (degrees)")
     plt.grid(True)
     plt.savefig(os.path.join(output_dir, file_prefix + "-direction-delta.png"))
-    # TODO: combined plot
+
+def plot_action_difference_map(
+    env,
+    control_policy,
+    trial_policy,
+    title_prefix,
+    file_prefix,
+    output_dir,
+):
+    observation_space = env.observation_space
+    base_observation = observation_space.sample()
+    STEPS = 100
+    XMIN = -3.8
+    XMAX = 3.8
+    norm_diffs = np.zeros((STEPS, STEPS))
+    x_diffs = np.zeros((STEPS, STEPS))
+    y_diffs = np.zeros((STEPS, STEPS))
+    direction_diffs = np.zeros((STEPS, STEPS))
+    for row in range(STEPS):
+        for col in range(STEPS):
+            x = XMIN + col*(XMAX - XMIN)/STEPS
+            y = XMIN + row*(XMAX - XMIN)/STEPS
+            # https://robotics.farama.org/envs/maze/point_maze/#observation-space
+            observation = {
+                'desired_goal':base_observation['desired_goal'],
+                # x and y velocities are 0
+                'observation':np.array([x, y, 0, 0]),
+                #'achieved_goal':base_observation['achieved_goal']
+                'achieved_goal':np.array([x, y])
+            }
+            control_action, _ = control_policy.predict(observation, deterministic=True)
+            trial_action, _ = trial_policy.predict(observation, deterministic=True)
+            norm_diffs[row][col] = norm(control_action) - norm(trial_action)
+            x_diffs[row][col] = control_action[0] - trial_action[0]
+            y_diffs[row][col] = control_action[1] - trial_action[1]
+            direction_diffs[row][col] = angle_between(control_action, trial_action)
+    def point2index(value):
+        return int(STEPS*(value - XMIN)/(XMAX - XMIN))
+    def plot_map(values):
+        plt.imshow(values)
+        plt.colorbar()
+        x_goal = point2index(base_observation['desired_goal'][0])
+        y_goal = point2index(base_observation['desired_goal'][1])
+        plt.plot(x_goal, y_goal, 'rx', markersize=4)
+
+    plot_map(norm_diffs)
+    plt.title(f"{title_prefix} Norm of Force Delta")
+    plt.savefig(os.path.join(output_dir, file_prefix + "-norm-force-delta.png"))
+
+    plt.clf()
+    plot_map(x_diffs)
+    plt.title(f"{title_prefix} X-Force Delta")
+    plt.savefig(os.path.join(output_dir, file_prefix + "-x-force-delta.png"))
+
+    plt.clf()
+    plot_map(y_diffs)
+    plt.title(f"{title_prefix} Y-Force Delta")
+    plt.savefig(os.path.join(output_dir, file_prefix + "-y-force-delta.png"))
+
+    plt.clf()
+    plot_map(direction_diffs)
+    plt.title(f"{title_prefix} Force Direction Delta (degrees)")
+    plt.savefig(os.path.join(output_dir, file_prefix + "-direction-delta.png"))
 
 
 if __name__ == "__main__":
@@ -177,20 +239,28 @@ if __name__ == "__main__":
     file_prefix = f"{control_config.maze_size} {control_config.environment} {control_config.algorithm} vs {trial_config.environment} {trial_config.algorithm}"
     # It only makes sense to compare distributions for PPO because DDPG is
     # deterministic.
-    if control_config.algorithm == "PPO" and trial_config.algorithm == "PPO":
-        plot_kl_divergence_distribution(
-            env.observation_space,
-            control_agent.policy,
-            trial_agent.policy,
-            title_prefix=title_prefix,
-            file_prefix=file_prefix,
-            output_dir=args.output_dir,
-        )
-    plot_action_difference_distribution(
-        env.observation_space,
+    #if control_config.algorithm == "PPO" and trial_config.algorithm == "PPO":
+    #    plot_kl_divergence_distribution(
+    #        env.observation_space,
+    #        control_agent.policy,
+    #        trial_agent.policy,
+    #        title_prefix=title_prefix,
+    #        file_prefix=file_prefix,
+    #        output_dir=args.output_dir,
+    #    )
+    #plot_action_difference_distribution(
+    #    env.observation_space,
+    #    control_agent.policy,
+    #    trial_agent.policy,
+    #    title_prefix=title_prefix,
+    #    file_prefix=file_prefix,
+    #    output_dir=args.output_dir,
+    #)
+    plot_action_difference_map(
+        env,
         control_agent.policy,
         trial_agent.policy,
         title_prefix=title_prefix,
-        file_prefix=file_prefix,
+        file_prefix=file_prefix + '-map',
         output_dir=args.output_dir,
     )
